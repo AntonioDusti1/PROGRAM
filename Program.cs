22d4b93e6a033818f2c23e15e3e93b0251ec3461
AntonioDusti1 <152974953+AntonioDusti1@users.noreply.github.com>
2024-01-13 12:48:21 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int ukupnaVrijednost = 0;
+
+        while (ukupnaVrijednost < 31)
+        {
+            Console.Write("Unesite vrijednost karte (1 - 13): ");
+            if (int.TryParse(Console.ReadLine(), out int vrijednostKarte))
+            {
+                if (vrijednostKarte < 1 || vrijednostKarte > 13)
+                {
+                    Console.WriteLine("Neispravna vrijednost karte. Unesite ponovno.");
+                    continue;
+                }
+
+                ukupnaVrijednost += vrijednostKarte;
+                Console.WriteLine($"Trenutna ukupna vrijednost karata: {ukupnaVrijednost}");
+            }
+            else
+            {
+                Console.WriteLine("Neispravan unos. Molimo unesite broj.");
+            }
+        }
+
+        if (ukupnaVrijednost == 31)
+        {
+            Console.WriteLine("Pobijedili ste!");
+        }
+        else
+        {
+            Console.WriteLine("Izgubili ste. Ukupna vrijednost karata je veća od 31.");
+        }
+
+        Console.ReadLine(); // Ova linija služi da zadrži konzolu otvorenom nakon izvršenja programa.
+    }
+}
